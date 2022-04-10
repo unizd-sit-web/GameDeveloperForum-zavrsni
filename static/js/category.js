46e03f06e71ac603f2c458456c1607d139840f55
mate lepur <mate.lepur123@gmail.com>
2022-04-10 20:42:36 +0200

    revert category page to dynamic content loading

--- static/js/category.js
+++ static/js/category.js
@@ -1,43 +1,61 @@
-import {getThreadsIds, getThreadByIdFiltered, getCategoryByIdFiltered} from "./storage.js"
-import {createThreadCard} from "./util.js"
+import {API_BASE_URL, createThreadCard, STATIC_BASE_URL} from "./util.js"
 
-// load navbar template
-$("#navbar-placeholder").load("navbar.html");
-// load footer template
-$("#footer-placeholder").load("footer.html");
+const threadContainer = $("#thread-container")
+const noThreadsLabel = $("#no-threads-label")
+const categoryEndpointUrl = removeCidFromUrl(API_BASE_URL + window.location.href.replace(/^.*\/\/[^\/]+/, '').replace("/threads", ""))
+const threadEndpointUrl = API_BASE_URL + window.location.href.replace(/^.*\/\/[^\/]+/, '')
+const staticThreadEndpointUrl = STATIC_BASE_URL + window.location.href.replace(/^.*\/\/[^\/]+/, '')
+const categoryId = window.location.href.replace(/^.*\/\/[^\/]+/, '').replace("/threads", "").split("/").pop()
 
-// constants
-const searchParams = new URLSearchParams(window.location.search);
-const sectionId = searchParams.get("section_id");
-const categoryId = searchParams.get("category_id");
-const noThreadsLabel = $("#no-threads-label");
-const threadContainer = $("#thread-container");
-const categoryTitle = $("#category-title");
-
-async function fetchTitle(){
-    let category = await getCategoryByIdFiltered(sectionId, categoryId, ["title"]).catch(console.error);
-    categoryTitle.text(category["title"]);
+function removeCidFromUrl(url){
+    let arr = url.split("/")
+    arr.pop()
+    return arr.join("/")
 }
 
-async function fetchThreads(){
-    let ids = await getThreadsIds(sectionId, categoryId, 10).catch(console.error);
-    if (ids.length > 0){
-        noThreadsLabel.hide();
-    }
-    for (let id of ids){
-        let thread = await getThreadByIdFiltered(sectionId, categoryId, id, ["title"]).catch(console.error);
-        let queryString = `section_id=${sectionId}&category_id=${categoryId}&thread_id=${id}`;
-        let card = createThreadCard(thread["title"], "./thread.html?" + queryString);
-        threadContainer.append(card);
-    }
-}
-
-fetchTitle().catch(console.error);
-fetchThreads().catch(console.error);
 $("#new-thread-btn").click(() => {
-    var searchParams = new URLSearchParams(window.location.search)
-    searchParams.set("section_id", sectionId)
-    searchParams.set("category_id", categoryId)
-    searchParams.set("redir", window.location.href)
-    window.location.href = "./new_thread.html?" + searchParams.toString()
+    let params = new URLSearchParams(window.location.search)
+    params.set("redir_url", window.location.href)
+    params.set("post_route", threadEndpointUrl + "/<tid>/posts")
+    params.set("thread_route", threadEndpointUrl)
+    window.location.href = STATIC_BASE_URL +  window.location.href.replace(/^.*\/\/[^\/]+/, '') + "/new?" + params.toString()
+})
+
+async function loadTitle(){
+    let titleRaw = await fetch(categoryEndpointUrl + "?cid=" + categoryId, {
+        "method": "GET",
+        "mode": "cors",
+        "Access-Control-Allow-Origin": "*"
+    })
+    let json = await titleRaw.json()
+    let title = json["categories"][0]["title"]
+    $("#category-title").text(title)
+}
+
+async function loadThreads(){
+    let threadsRaw = await fetch(threadEndpointUrl, {
+        "method": "GET",
+        "mode": "cors",
+        "Access-Control-Allow-Origin": "*"
+    })
+    let json = await threadsRaw.json()
+    let threads = json["threads"]
+    if (threads.length > 0){
+        noThreadsLabel.hide()
+    } else {
+        noThreadsLabel.show()
+    }
+    for (let thread of json["threads"]){
+        let card = createThreadCard(thread["title"], staticThreadEndpointUrl + "/" + thread["thread_id"] + "/posts")
+        threadContainer.append(card)
+    }
+}
+
+loadTitle().catch((err) => {
+    console.log(err)
+    alert("Failed to load title")
+})
+loadThreads().catch((err) => {
+    console.error(err)
+    alert("Failed to load threads")
 })
