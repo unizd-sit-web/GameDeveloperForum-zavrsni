a6045e5ab0b7664dba40031204fbe0083c974e80
mate lepur <mate.lepur123@gmail.com>
2022-04-10 20:42:36 +0200

    modified new_thread page to use new backend routes

--- static/js/new_thread.js
+++ static/js/new_thread.js
@@ -1,29 +1,57 @@
-import {createThread, createPost} from "./storage.js"
-
-// load navbar template
-$("#navbar-placeholder").load("navbar.html");
-// load footer template
-$("#footer-placeholder").load("footer.html");
-
 // constants
 const titleField = $("#title-field");
 const contentField = $("#content-field");
-const btn = $("#new-thread-btn");
-const searchParams = new URLSearchParams(window.location.search);
-const sectionId = searchParams.get("section_id");
-const categoryId = searchParams.get("category_id");
-const redirUrl = searchParams.get("redir");
 
-btn.click(() => {
+async function createThread(route, title){
+    let res = await fetch(route, {
+        method: "POST",
+        body: JSON.stringify({"title": title}),
+        headers: {
+            "Content-Type": "application/json"
+        }
+    })
+    let json = await res.json()
+    if (res.status != 201){
+        return Promise.reject(json["error"])
+    }
+    return json["new_thread_id"]
+}
+
+async function createPost(route, content){
+    let res = await fetch(route, {
+        method: "POST",
+        body: JSON.stringify({"content": content}),
+        headers: {
+            "Content-Type": "application/json"
+        }
+    })
+    let json = await res.json()
+    if (res.status != 201){
+        return Promise.reject(json["error"])
+    }
+    return json["new_thread_id"]
+}
+
+async function sendPostRequests(thread_route, post_route, thread_title, post_content){
+    let tid = await createThread(thread_route, thread_title).catch((err) => {
+        return Promise.reject()
+    })
+    await createPost(post_route.replace("<tid>", tid), post_content)
+}
+
+$("#new-thread-btn").click(() => {
     let title = titleField.val();
     let content = contentField.val();
     if (!(title.length > 0 && content.length > 0)) {
         alert("Title and content cannot be empty!");
         return
     }
-    createThread(sectionId, categoryId, title).then((tid) => {
-        createPost(sectionId, categoryId, tid, content, "Admin").then((pid) => {
-            window.location.href = redirUrl
-        }).catch(console.error);
-    }).catch(console.error);
+    // POST thread and post to backend
+    let params = new URLSearchParams(window.location.search)
+    sendPostRequests(params.get("thread_route"), params.get("post_route"), title, content)
+    .then(() => {
+        window.location.href = params.get("redir_url")
+    }).catch((err) => {
+        alert("Failed to create thread")
+    })
 })
