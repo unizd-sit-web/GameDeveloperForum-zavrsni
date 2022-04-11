815d9dd87ce1d478f7756af52c4a128aa03a8393
MRumstajn <mrumstajn@student.unizd.hr>
2022-04-11 12:17:59 +0200

    replaced index.html and index.js with the correct file version

--- static/js/index.js
+++ static/js/index.js
@@ -1,51 +1,38 @@
-import { getSectionIdByTitle, getCategoriesIds, getThreadsIds, getThreadByIdFiltered } from './storage.js';
-import { createThreadCard } from "./util.js"
+import {API_BASE_URL, STATIC_BASE_URL, createThreadCard} from "./util.js";
 
-// load navbar template
-$("#navbar-placeholder").load("navbar.html");
-// load footer template
-$("#footer-placeholder").load("footer.html");
-
-// constants
 const threadContainer = $("#thread-container")
+const staticThreadEndpointUrl = STATIC_BASE_URL + "/news/categories/news-category/threads"
+const threadEndpointUrl = API_BASE_URL + "/news/categories/news-category/threads"
 const noThreadsLabel = $("#no-threads-label")
 
-let sectionId
-let categoryId
-/**
- * Fetches and caches the id of the section this page belongs to and the id of the category that should be displayed
- */
-async function fetchids() {
-    sectionId = await getSectionIdByTitle("News").catch(console.error)
-    let ids = await getCategoriesIds(sectionId, 1).catch(console.error)
-    categoryId = ids[0]
-}
-
-/**
- * Fetches and displays threads from the category
- */
-async function loadThreads() {
-    let threadsIds = await getThreadsIds(sectionId, categoryId, 10).catch(console.error)
-    if (threadsIds.length > 0) {
-        noThreadsLabel.remove()
-    }
-    for (let threadId of threadsIds) {
-        let threadData = await getThreadByIdFiltered(sectionId, categoryId, threadId, ["title"]).catch(console.error)
-        let queryString = `section_id=${sectionId}&category_id=${categoryId}&thread_id=${threadId}`
-        let threadCard = createThreadCard(threadData["title"], "./thread.html?" + queryString)
-        threadContainer.append(threadCard)
-    }
-}
-
-fetchids().then(() => {
-    loadThreads().catch(console.error)
-    // new thread button redirects to a form page and supplies the section and category id via urlquery
-    $("#new-thread-btn").click(() => {
-        var searchParams = new URLSearchParams(window.location.search)
-        searchParams.set("section_id", sectionId)
-        searchParams.set("category_id", categoryId)
-        searchParams.set("redir", window.location.href)
-        window.location.href = "./new_thread.html?" + searchParams.toString()
-    })
+$("#new-thread-btn").click(() => {
+    let params = new URLSearchParams(window.location.search)
+    params.set("redir_url", window.location.href)
+    params.set("thread_route", "/api/news/categories/news-category/threads")
+    params.set("post_route", "/api/news/categories/news-category/threads/<tid>/posts")
+    window.location.href = "/news/categories/news-category/threads/new?" + params.toString()
 })
 
+async function loadThreads(){
+    let threadsRaw = await fetch(threadEndpointUrl, {
+        "method": "GET",
+        "mode": "cors",
+        "Access-Control-Allow-Origin": "*"
+    })
+    let json = await threadsRaw.json()
+    let threads = json["threads"]
+    if (threads.length > 0){
+        noThreadsLabel.hide()
+    } else {
+        noThreadsLabel.show()
+    }
+    for (let thread of threads){
+        let card = createThreadCard(thread["title"], staticThreadEndpointUrl + "/" + thread["thread_id"] + "/posts")
+        threadContainer.append(card)
+    }
+}
+
+loadThreads().catch((err) => {
+    console.error(err)
+    alert("Failed to load threads")
+})
